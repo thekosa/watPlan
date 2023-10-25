3a9350ae7b51c0a4934f6b6f821654fabe3114ac
Michał Kosieradzki <49490562+thekosa@users.noreply.github.com>
2023-10-26 00:35:03 +0200

    Main flow implemented

--- src/main/java/pl/kosieradzki/Main/Main.java
+++ src/main/java/pl/kosieradzki/Main/Main.java
@@ -1,12 +1,54 @@
 package pl.kosieradzki.Main;
 
+import com.google.api.services.calendar.model.CalendarListEntry;
+import org.jsoup.select.Elements;
+import pl.kosieradzki.Calendar.CalendarEventEntity;
+import pl.kosieradzki.Calendar.CalendarHandler;
+import pl.kosieradzki.FileHandler;
+import pl.kosieradzki.Lessons.Lessons;
+import pl.kosieradzki.Lessons.LessonsConverter;
 import pl.kosieradzki.Lessons.LessonsFilter;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws GeneralSecurityException, IOException {
         System.out.println("Hello world!");
 
+        final String configFilePath = "src/main/resources/config/";
+        final String filtersFilePath = "src/main/resources/config/filters/";
 
-        LessonsFilter lessonsFilter = new LessonsFilter();
+        FileHandler fileHandler = new FileHandler();
+
+        List<String> groups = fileHandler.readFile(configFilePath + "groups");
+
+        for (String group : groups) {
+            Lessons lessons = new Lessons(group);
+            Elements lessonsNotFiltered = lessons.getLessons();
+
+            List<String> groupFilter = fileHandler.readFile(filtersFilePath + group);
+            LessonsFilter lessonsFilter = new LessonsFilter(groupFilter);
+            Elements lessonsFiltered = lessonsFilter.filterOut(lessonsNotFiltered);
+
+            LessonsConverter lessonsConverter = new LessonsConverter();
+            List<CalendarEventEntity> lessonsConverted = lessonsConverter.elements2CEE(lessonsFiltered);
+
+            CalendarHandler calendarHandler = new CalendarHandler();
+            List<CalendarListEntry> calendarsList = calendarHandler.getCalendars();
+
+            for (CalendarListEntry calendar : calendarsList) {
+                if (calendar.getSummary().equals(group)) {
+                    calendarHandler.deleteCalendar(calendar.getId());
+                    break;
+                }
+            }
+
+            String calendarId = calendarHandler.insertCalendar(group);
+
+            calendarHandler.insertEvents(lessonsConverted, calendarId);
+        }
     }
 }
